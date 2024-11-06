9130550e2287b944a8bf60a45e9c61479d9cfcd4
Žan Grčar <grcar.zan1@gmail.com>
2024-11-06 14:50:56 +0100

    Testing graphs.

--- go.mod
+++ go.mod
@@ -1,8 +1,19 @@
 module github.com/jmCodeCraft/go-network
 
-go 1.21.1
+go 1.23
+
+toolchain go1.23.2
 
 require (
 	github.com/jinzhu/copier v0.4.0
 	github.com/mroth/weightedrand v1.0.0
+)
+
+require github.com/nvkp/turtle v1.2.1
+
+require (
+	github.com/PuerkitoBio/goquery v1.10.0 // indirect
+	github.com/andybalholm/cascadia v1.3.2 // indirect
+	github.com/knakk/rdf v0.0.0-20190304171630-8521bf4c5042 // indirect
+	golang.org/x/net v0.29.0 // indirect
 )
--- main.go
+++ main.go
@@ -2,12 +2,98 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"net/http"
+	"os"
 
+	"github.com/PuerkitoBio/goquery"
 	"github.com/jmCodeCraft/go-network/model"
+	"github.com/nvkp/turtle"
 )
 
+//"github.com/jmCodeCraft/go-network/model"
+
+type Triple struct {
+	Subject   string `turtle:"subject"`
+	Predicate string `turtle:"predicate"`
+	Object    string `turtle:"object"`
+}
+
+func GetKeywordsAndAbstractEmbedding(url string) (string, []string, error) {
+	res, err := http.Get(url)
+	if err != nil {
+		return "", nil, err
+	}
+	defer res.Body.Close()
+
+	doc, err := goquery.NewDocumentFromReader(res.Body)
+	if err != nil {
+		return "", nil, err
+	}
+
+	fmt.Println("aaa \n", res.Body, doc)
+	return "", nil, nil
+}
+
 func main() {
-	a := model.NewGraph{
+
+	file, err := os.Open("citation_network_tiny.ttl")
+	if err != nil {
+		fmt.Println("Error opening file:", err)
+		return
+	}
+	defer file.Close()
+
+	var triples []Triple
+
+	byteFile, err := io.ReadAll(file)
+	if err != nil {
+		fmt.Println("Error converting file:", err)
+		return
+	}
+	err = turtle.Unmarshal(byteFile, &triples)
+	if err != nil {
+		fmt.Println("Error unmarshaling Turtle data:", err)
+		return
+	}
+
+	a, _, _ := GetKeywordsAndAbstractEmbedding(triples[0].Subject)
+
+	fmt.Println(a)
+
+	g := model.NewGraph{
+		Nodes: map[string]model.NewNode{},
+		Edges: map[int]model.NewEdge{},
+		Type:  "graph",
+	}
+
+	for _, triple := range triples {
+		value, ok := g.Nodes[triple.Subject]
+		value2, ok2 := g.Nodes[triple.Object]
+		if !ok {
+			value = model.NewNode{
+				ID:         triple.Subject,
+				Attributes: map[string]interface{}{},
+			}
+			g.AddNode(value)
+		}
+		if !ok2 {
+			value2 = model.NewNode{
+				ID:         triple.Object,
+				Attributes: map[string]interface{}{},
+			}
+			g.AddNode(value2)
+		}
+		g.AddEdge(model.NewEdge{
+			First_node:  value,
+			Second_node: value2,
+			Attributes:  map[string]interface{}{},
+		})
+	}
+
+	fmt.Printf("%d, %d\n", len(g.Nodes), len(g.Edges))
+
+	/*a := model.NewGraph{
 		Nodes: map[string]model.NewNode{},
 		Edges: map[int]model.NewEdge{},
 		Type:  "graph",
@@ -36,5 +122,5 @@ func main() {
 
 	a.AddNodeAttribute("node_1", "stacionary_vision", 15)
 
-	fmt.Println(a.ToString())
+	fmt.Println(a.ToString())*/
 }
